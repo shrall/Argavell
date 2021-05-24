ba9d639fa6338fa8ac2ae9a9fca94565f2cd88cf
shrall <shrallvierdo@gmail.com>
2021-05-24 15:15:53 +0700

    navbar

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,70 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('images/ArgavellLogo.png') }}" alt="" width="50" height="50"
+                class="d-inline-block align-text-top">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav me-auto">
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item mx-4">
+                    <a href="#" class="text-argavell text-decoration-none font-proxima-nova font-weight-bold cursor-pointer">
+                        Argan Oil
+                    </a>
+                </li>
+                <li class="nav-item mx-4">
+                    <a href="#" class="text-argavell text-decoration-none font-proxima-nova font-weight-bold cursor-pointer">
+                        Argan Shampoo
+                    </a>
+                </li>
+                <li class="nav-item mx-4">
+                    <a href="#" class="text-argavell text-decoration-none font-proxima-nova font-weight-bold cursor-pointer">
+                        Kleanse
+                    </a>
+                </li>
+                <li class="nav-item mx-4">
+                    <a href="#" class="text-argavell text-decoration-none font-proxima-nova font-weight-bold cursor-pointer">
+                        Contact Us
+                    </a>
+                </li>
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item mx-4">
+                            <a class="text-argavell text-decoration-none font-proxima-nova font-weight-bold"
+                                href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item mx-4">
+                        <a class="nav-link" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                                                                             document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
+                @endguest
+                <li class="nav-item mx-4">
+                    <a href="#" class="text-argavell text-decoration-none font-proxima-nova font-weight-bold cursor-pointer">
+                        <span class="fa fa-fw fa-shopping-cart"></span>
+                    </a>
+                </li>
+                <li class="nav-item mx-4">
+                    <a href="#" class="text-argavell text-decoration-none font-proxima-nova font-weight-bold cursor-pointer">
+                        <span class="fa fa-fw fa-search"></span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,53 +33,7 @@
 <body>
     <div id="app">
         @if (Route::current()->getName() != 'register' && Route::current()->getName() != 'login' && Route::current()->getName() != 'password.request' && Route::current()->getName() != 'password.reset')
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto">
-
-                        </ul>
-
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                            <!-- Authentication Links -->
-                            @guest
-                                @if (Route::has('login'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                    </li>
-                                @endif
-
-                                @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                    </li>
-                                @endif
-                            @else
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                                                                 document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </li>
-                            @endguest
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+        @include('inc.navbar')
         @endif
 
         @if (Route::current()->getName() == 'register' || Route::current()->getName() == 'login' || Route::current()->getName() == 'password.request' || Route::current()->getName() == 'password.reset')
