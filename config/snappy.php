56ebdf8cd0369a689838217defa114a4b53ad1ec
shrall <shrallvierdo@gmail.com>
2021-12-22 04:37:27 +0700

    Update snappy.php

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
 
     'pdf' => [
         'enabled' => true,
-        'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64').' --viewport-size 1024x768',
+        'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         'timeout' => false,
         'options' => [],
         'env'     => [],
